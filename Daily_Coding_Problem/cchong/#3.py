48db404e3a281e74d39bf022ddb82becd3f8c1b1
Christine Chong <Christine_Chong@trimble.com>
2019-07-15 14:53:37 -0400

    Finished 3

--- Daily_Coding_Problem/cchong/#3.py
+++ Daily_Coding_Problem/cchong/#3.py
@@ -1,11 +1,31 @@
+# Brute Force Method
+# def twosum(in_arr, k):
+#     out = 0
+#     for i in range(len(in_arr)-1):
+#         for j in range(len(in_arr)-1):
+#             if(in_arr[i]+in_arr[len(in_arr)-(j+1)] == k):
+#                 out = 1
+#                 break
+#             break
+#
+#     print(out)
+#
+# twosum([1,3,4,2],6)
+
+#One Pass Method
 def twosum(in_arr, k):
     out = 0
-    for i in range(len(in_arr)-1):
-        for j in range(len(in_arr)-1):
-            if(in_arr[i]+in_arr[len(in_arr)-(j+1)] == k):
-                out = 1
+    hash = []
+    for i in in_arr:
+        complement = k - i
+        try :
+            hash.index(complement)
+            out = 1
+        except (ValueError) as e:
+            pass
+        hash.append(i)
+    return out
 
-    print(out)
 
-twosum([9,10,15,7,3],24)
+print(twosum([1,99,49,2],100))
 
