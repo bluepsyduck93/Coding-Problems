8b7991f7c2bc4288bf90646a73e5dc9264a78894
Christine Chong <Christine_Chong@trimble.com>
2019-07-17 11:38:30 -0400

    Added If Else block alternative solution

--- AlgoExpert/cchong/easy 1.py
+++ AlgoExpert/cchong/easy 1.py
@@ -14,4 +14,24 @@ def twoNumberSum(in_arr, k):
         hash.append(i)
     return sorted(out)
 
+# Sorted uses TimSort
+# https://en.wikipedia.org/wiki/Timsort
 print(twoNumberSum([3,5,-4,8,11,1,-1,6],10))
+
+# def twoNumberSum(in_arr, k):
+#     out = []
+#     hash = []
+#     for i in in_arr:
+#         complement = k - i
+#         try :
+#             hash.index(complement)
+#             if complement < i:
+#                 out.append(in_arr[hash.index(complement)])
+#                 out.append(i)
+#             else:
+#                 out.append(i)
+#                 out.append(in_arr[hash.index(complement)])
+#         except (ValueError) as e:
+#             pass
+#         hash.append(i)
+#     return sorted(out)
