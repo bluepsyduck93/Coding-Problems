55cee2e212db560f2e76402b0cac0e79cf5fa9aa
Christine Chong <Christine_Chong@trimble.com>
2019-08-07 20:13:32 -0400

    Added dynamic solution for 32

--- Daily_Coding_Problem/cchong/#32.py
+++ Daily_Coding_Problem/cchong/#32.py
@@ -1,14 +1,35 @@
-# O(n) solution
-def editDist(string1, string2):
-    string1 = list(string1)
-    string2 = list(string2)
-    min = len(string1) if len(string1) < len(string2) else len(string2)
-    max = len(string2) if len(string1) < len(string2) else len(string2)
-    samechar = 0
-    for i in range(0, min):
-        if string1[i] == string2[i]:
-            samechar = samechar +1
+#O 2^n
+# def editDist(str1, str2, m, n):
+#     if m == 0 or n == 0:
+#         return 0;
+#     elif str1[m-1] == str2[n-1]:
+#         return 1 + editDist(str1,str2,m-1,n-1);
+#     else:
+#         return max(editDist(str1, str2, m, n-1), editDist(str1, str2, m-1, n));
+#
+#
+# str1 = "applepen"
+# str2 = "pen"
+# print(editDist(str1, str2, len(str1), len(str2)))
 
-    print(max-samechar)
+#O(m*n)
 
-editDist("kitten", "sitting")
+def editDist(str1, str2, m, n):
+    L = [[0 for i in range(n+1)]
+            for i in range(m+1)]
+    for i in range(m+1):
+        for j in range(n+1):
+            if i==0 or j==0:
+                L[i][j] = 0
+            elif str1[i-1] == str2[j-1]:
+                L[i][j] = L[i-1][j-1]+1
+            else:
+                L[i][j] = max(L[i-1][j], L[i][j-1])
+    print(L)
+    maxlen = len(str1) if len(str1) > len(str2) else len(str2)
+    return maxlen - L[m][n]
+
+
+str1 = "pen"
+str2 = "apen"
+print(editDist(str1, str2, len(str1), len(str2)))
